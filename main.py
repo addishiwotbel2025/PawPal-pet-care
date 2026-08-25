262a87e4977412fde41d485f0206d26c7d9e6613
Addis Dagnew <addishiwotbel22@gmail.com>
2026-08-24 23:00:28 -0400

    Refactor main function and clean up code

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from models import Owner, Pet, Task, Priority
 from scheduler import Scheduler
 
-
 def main():
-
     owner = Owner("Addis", 60)                       # ← the owner (time budget)
     albert = Pet("Albert", 2, "Ragdoll", "Cat")
     albert.add_task(Task("Morning walk", 30, Priority.HIGH))
@@ -11,19 +9,6 @@ def main():
     scheduler = Scheduler(owner, albert.tasks)       # owner first, pet's tasks second
     plan = scheduler.build_plan()
     print(plan.explain())
-
-
-
-
+    
 if __name__ == "__main__":
     main()
-
-
-'''
-what feels manual?
-
-    i have to put in morning walk.
-    i have to track times that are clashing with eachother
-    i choose my preferences on my own
-    
-'''
